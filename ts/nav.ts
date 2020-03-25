c2aeeae8868372be941e12fa877ca8a288364ea1
IB-Code <code@innovatebham.com>
2020-03-25 18:41:40 -0500

    removed console.log

--- ts/nav.ts
+++ ts/nav.ts
@@ -7,7 +7,6 @@ window.addEventListener('DOMContentLoaded', () => {
     desktopSlideOut.style.display = 'none';
 
     // check screen width and set data target of menu button.
-    console.log(screen.availWidth);
     if (screen.availWidth >= 1024) {
         button.attributes[4].value = '#mainDesktopNavbar';
     } else {
