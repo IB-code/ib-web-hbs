9d60e8ffff54127e1e6c41971648f262891a8f53
IB-Code <josh@innovatebham.com>
2020-05-12 14:45:47 -0500

    removed COVID-19 modal

--- ts/home.ts
+++ ts/home.ts
@@ -1,6 +1,6 @@
-let covidModal = $('#covid-modal');
+// let covidModal = $('#covid-modal');
 
-if (localStorage.getItem('covid-modal') === null) {
-    covidModal.modal('show');
-    localStorage.setItem('covid-modal', 'true');
-}
+// if (localStorage.getItem('covid-modal') === null) {
+//     covidModal.modal('show');
+//     localStorage.setItem('covid-modal', 'true');
+// }
