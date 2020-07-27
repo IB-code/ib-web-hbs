1572dd644dffe94340ebf565df0c1d0767518459
IB-Code <josh@innovatebham.com>
2020-07-26 20:44:19 -0500

    fixed mobile nav.

--- ts/nav.ts
+++ ts/nav.ts
@@ -1,14 +1,20 @@
 window.addEventListener('DOMContentLoaded', () => {
     const desktopSlideOut = document.getElementById('mainDesktopNavbar'),
         button = document.getElementById('mainButton'),
-        closeNavButton = document.getElementById('closeNavButton');
+        closeNavButton = document.getElementById('closeNavButton'),
+        donateBtn = document.getElementById("donate-btn-nav");
 
     // start the desktop navbar as hidden.
     desktopSlideOut.style.display = 'none';
 
-    // check screen width and set data target of menu button.
-    if (screen.availWidth >= 1024) {
+    // start donate button as hidden.
+    donateBtn.style.display = "none";
+
+    // check screen width and set data target of menu button.show donate on desktop.
+     if (screen.availWidth >= 1024) {
         button.attributes[4].value = '#mainDesktopNavbar';
+        donateBtn.style.display = "block";
+
     } else {
         button.attributes[4].value = '#mainMobileNavbar';
     }
