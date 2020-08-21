4181cb889018a3f337d29387c03335999a8aca8d
IB-Code <josh@innovatebham.com>
2020-08-21 09:02:07 -0500

    updating cors pls

--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -4,12 +4,12 @@ const { HTTP_SUCCESS } = HTTP_CODES;
 
 export default (err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
     // CORS headers
-    res.header('Access-Control-Allow-Origin', 'https://innovatebham.agsprime.net | *');
+    res.header('Access-Control-Allow-Origin', 'https://innovatebham.agsprime.net');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 
     // Set custom headers for CORS
     res.header('Access-Control-Allow-Headers',
-        'Content-type,Accept,X-Access-Token,X-Key');
+        'Content-Type,Accept,X-Access-Token,X-Key');
 
     if (req.method === 'OPTIONS') {
         res.status(HTTP_SUCCESS).end();
