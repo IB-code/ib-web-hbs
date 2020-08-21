1fc8e60608c1b16023ebdf95cbbfd6afc480f20a
IB-Code <josh@innovatebham.com>
2020-08-21 09:42:04 -0500

    reverting cors to original minus the fixing of content-type casing in setting of headers in cors.ts

--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -5,12 +5,11 @@ const { HTTP_SUCCESS } = HTTP_CODES;
 export default (err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
     // CORS headers
     res.header('Access-Control-Allow-Origin', '*');
-    res.header("Access-Control-Allow-Credentials", 'true');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 
     // Set custom headers for CORS
     res.header('Access-Control-Allow-Headers',
-        'Content-Type,Accept,X-Access-Token,X-Key');
+        'Content-type,Accept,X-Access-Token,X-Key');
 
     if (req.method === 'OPTIONS') {
         res.status(HTTP_SUCCESS).end();
