fa7e3610291c02dd4f60ffda6afbe318619f3f82
IB-Code <josh@innovatebham.com>
2020-11-03 20:21:36 -0600

    worked on spinner

--- ts/apply.ts
+++ ts/apply.ts
@@ -1,3 +1,4 @@
 document.getElementById('apply-iframe').onload = () => {
     document.getElementById("spinner-container").remove();
+    document.getElementById("apply-iframe").style.height = "100vh";
 };
