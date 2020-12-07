5fce1942d9720cc2021c29663c172dc6dd3e1f81
IB-Code <josh@innovatebham.com>
2020-12-07 14:23:09 -0600

    fixing application link

---
+++ ts/donate.ts
@@ -0,0 +1,4 @@
+document.getElementById('dv-iframe').onload = () => {
+    document.getElementById("spinner-container").remove();
+    document.getElementById('dv-iframe').style.height = "100vh";
+};
