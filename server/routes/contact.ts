a31202ab06bc7066da6fc99556fb3ca5978a1d3a
IB-Code <josh@innovatebham.com>
2020-12-09 11:50:47 -0600

    attempt to fix contact reroute error

--- server/routes/contact.ts
+++ server/routes/contact.ts
@@ -100,11 +100,11 @@ export function handleFormSubmission(
             },
         )
         .then((res) => {
-            res.redirect('https://innovatebham.com/contact?response=thanks');
+            res.redirect('innovatebham.com/contact?response=thanks');
         })
         .catch((err) => {
             console.log('err', err);
-            res.redirect('https://innovatebham.com/contact?response=whoops');
+            res.redirect('innovatebham.com/contact?response=whoops');
         });
 }
 
