5513648896f01c739c3055de6cd5dcdb9082a582
IB-Code <josh@innovatebham.com>
2020-12-14 09:26:47 -0600

    updated to preferred sendgrid npm package and updated server > mail.ts to use that

--- server/utils/mail.ts
+++ server/utils/mail.ts
@@ -1,43 +1,58 @@
-import * as sendgrid from 'sendgrid';
+import * as sendgrid from "@sendgrid/mail";
 
-let helper = sendgrid.mail;
-
-export function send(
+export async function send(
     from: string,
     to: string,
     subject: string,
     message: string,
 ): Promise<any> {
+    sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
     try {
-        let sg = sendgrid(process.env.SENDGRID_API_KEY);
-        let request = sg.emptyRequest({
-            method: 'POST',
-            path: '/v3/mail/send',
-            body: {
-                personalizations: [
-                    {
-                        to: [
-                            {
-                                email: to,
-                            },
-                        ],
-                        subject,
-                    },
-                ],
-                from: {
-                    email: from,
-                },
-                content: [
-                    {
-                        type: 'text/plain',
-                        value: message,
-                    },
-                ],
-            },
+        await sendgrid.send({
+            to: to,
+            from: from,
+            subject: subject,
+            text: message,
         });
+    } catch (error) {
+        console.error(error);
 
-        return sg.API(request);
-    } catch (err) {
-        console.log(err);
+        if (error.response) {
+            console.error(error.response.body)
+        }
     }
 }
+
+    // let request = sg.emptyRequest({
+    //     method: 'POST',
+    //     path: '/v3/mail/send',
+    //     body: {
+    //         personalizations: [
+    //             {
+    //                 to: [
+    //                     {
+    //                         email: to,
+    //                     },
+    //                 ],
+    //                 subject,
+    //             },
+    //         ],
+    //         from: {
+    //             email: from,
+    //         },
+    //         content: [
+    //             {
+    //                 type: 'text/plain',
+    //                 value: message,
+    //             },
+    //         ],
+    //     },
+    // });
+
+    // return sg.API(request)
+    //     .then(res => console.log(res.statusCode))
+    //     .catch(err => {
+    //         // error is an instance of SendGridError
+    //         // The full response is attached to error.response
+    //         console.log(err.response.statusCode)
+    //     });
