d1625e5d23f51c704c0fa97230e54a4b373a8f0b
IB-Code <josh@innovatebham.com>
2020-12-15 17:50:30 -0600

    removed unneeded code from mail.ts and added proc/dev check to https enforcement

--- server/utils/mail.ts
+++ server/utils/mail.ts
@@ -22,37 +22,3 @@ export async function send(
         }
     }
 }
-
-    // let request = sg.emptyRequest({
-    //     method: 'POST',
-    //     path: '/v3/mail/send',
-    //     body: {
-    //         personalizations: [
-    //             {
-    //                 to: [
-    //                     {
-    //                         email: to,
-    //                     },
-    //                 ],
-    //                 subject,
-    //             },
-    //         ],
-    //         from: {
-    //             email: from,
-    //         },
-    //         content: [
-    //             {
-    //                 type: 'text/plain',
-    //                 value: message,
-    //             },
-    //         ],
-    //     },
-    // });
-
-    // return sg.API(request)
-    //     .then(res => console.log(res.statusCode))
-    //     .catch(err => {
-    //         // error is an instance of SendGridError
-    //         // The full response is attached to error.response
-    //         console.log(err.response.statusCode)
-    //     });
