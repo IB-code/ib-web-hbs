619f72db2535468e35a2ee8b0a07b3c4a6a9cc87
IB-Code <josh@innovatebham.com>
2020-12-29 10:11:06 -0600

    mainly finished on both mobile and desktop. pushing to staging

--- ts/newsletter.ts
+++ ts/newsletter.ts
@@ -1,19 +1,63 @@
-// Container for newsletter
-const newsletterContainer = document.querySelector(".newsletter-container");
-// Container for newsletter signup circle
-const newsletterCircle = document.getElementById("newsletter-circle");
+// container for desktop newsletter
+const desktopNewsletterContainer = <HTMLDivElement>document.querySelector("#desktopNewsletter");
+// container for mobile newsletter
+const mobileNewsletterContainer = <HTMLDivElement>document.querySelector("#mobileNewsletter");
+// newsletter close button 
+const desktopCloseButton = <HTMLButtonElement>document.querySelector("#close-button");
+// container for newsletter signup circle. It's an icon to trigger the newsletter manually
+const newsletterCircle = <HTMLDivElement>document.querySelector("#newsletter-circle");
 // localStorage value used to track visits for the purpose hiding newsletter on visit schedule
-const newsLetterState = localStorage.getItem('newsletterVisitCount');
+const newsletterLastDisplayTime = Number(localStorage.getItem('last-visit'));
+// getting current time in milliseconds
+const currentTime = Date.now();
 
-// Setting newsletter value to localStorage if it doesn't exist
-let visitCount = 0; 
+// checking width of browser viewport to setup mobile version
+if (screen.availWidth < 769) {
+    // remove desktop newsletter from document
+    desktopNewsletterContainer.parentNode.removeChild(desktopNewsletterContainer);
 
-if (!newsLetterState) {
-    visitCount++;
-    localStorage.setItem('newsletterVisitCount', visitCount.toString());
-} else {
-    visitCount++;
+    const mobileCloseButton = <HTMLDivElement>document.querySelector("#mobile-close-button");
+    mobileCloseButton.setAttribute("data-toggle", "modal");
+    mobileCloseButton.setAttribute("data-target", "#mobileNewsletter");
+
+    // add attributes needed by Bootstrap to trigger modal
+    newsletterCircle.setAttribute("data-toggle", "modal");
+    newsletterCircle.setAttribute("data-target", "#mobileNewsletter");
+
+    newsletterCircle.style.height = "50px";
+    newsletterCircle.style.width = "50px";
+    newsletterCircle.style.borderRadius = "50px";
 }
 
-// adding class to trigger newsletter popup
-newsletterCircle.addEventListener("click", () => newsletterContainer.classList.add("animate-newsletter"));
+// open newsletter via animation if most recent visit was over 7 days ago
+if (!newsletterLastDisplayTime || currentTime - newsletterLastDisplayTime > 604800000) {
+    localStorage.setItem('last-visit', currentTime.toString());
+
+    if (screen.availWidth < 769) {
+        // adding class to desktop newsletter to trigger animation
+        desktopNewsletterContainer.classList.add("animate-newsletter");
+    }
+} else {
+    // showing icon on the page if user wishes to trigger newsletter manually. 
+    // setting display prop from none to flex
+    newsletterCircle.classList.add("d-flex", "justify-content-center", "align-items-center");
+}
+
+// close newsletter on click o "X" button and show newsletter icon to reopen
+desktopCloseButton.addEventListener('click', () => {
+    desktopNewsletterContainer.style.display = 'none';
+    newsletterCircle.classList.add("d-flex", "justify-content-center", "align-items-center")
+});
+
+// adding class to trigger newsletter popup and remove circle
+newsletterCircle.addEventListener('click', () => {
+    if (screen.availWidth > 769) {
+        desktopNewsletterContainer.style.display = 'block';
+        desktopNewsletterContainer.style.opacity = '1';
+        desktopNewsletterContainer.style.animation = 'none';
+
+        // setting display prop from none to flex
+        newsletterCircle.classList.remove("d-flex", "justify-content-center", "align-items-center");
+        newsletterCircle.style.display = 'none';
+    }
+});
