fc0d44c19e786a75d803c40fc6d19d3244c3dd1d
joshua-hurn <joshua-hurn@hotmail.com>
2021-02-05 00:35:17 -0600

    updated footer verbiage to match change to programs pages

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,8 +1,6 @@
 import * as express from 'express';
 import meta from '../middleware/meta';
 import * as index from './home';
-import * as about from './about';
-// import * as courses from './courses';
 import * as software from './software';
 import * as partners from './partners';
 import * as contact from './contact';
@@ -11,22 +9,24 @@ import * as certifications from './degrees-and-certs';
 import * as testimonials from './testimonials';
 import * as faq from './faq';
 import * as programs from './programs';
-import * as accelerated from './accelerated';
 import * as apprenticeship from './apprenticeship';
 import * as media from './media';
 import * as staff from './staff';
 import * as mission from './mission';
-import * as financials from './financials';
 import * as board from './board';
 import * as donate from './donate';
 import * as apply from './apply';
 import * as interestForm from "./interestForm";
 import * as careers from './careers';
 import * as PDAndSupport from "./profdev&supportserv";
-import * as blogList from "./bloglist";
-import * as tempBlog1 from "./blog-temp1";
-import * as tempBlog2 from "./blog-temp2";
-import * as tempBlog3 from "./blog-temp3";
+// import * as blogList from "./bloglist";
+// import * as tempBlog1 from "./blog-temp1";
+// import * as tempBlog2 from "./blog-temp2";
+// import * as tempBlog3 from "./blog-temp3";
+// import * as courses from './courses';
+// import * as accelerated from './accelerated';
+// import * as financials from './financials';
+// import * as about from './about';
 
 const router = express.Router();
 
@@ -43,7 +43,7 @@ router
     .get('/donate', donate.context, meta, donate.render)
     .get('/contact', contact.context, meta, contact.render)
     .post('/contact/submit', contact.handleFormSubmission)
-    .get('/programs', programs.context, meta, programs.render)
+    .get('/program', programs.context, meta, programs.render)
     .get('/apprenticeship', apprenticeship.context, meta, apprenticeship.render)
     .get('/degrees-and-certifications', certifications.context, meta, certifications.render)
     .get('/courses/software', software.context, meta, software.render)
@@ -51,7 +51,7 @@ router
     .get('/partners', partners.context, meta, partners.render)
     .get('/testimonials', testimonials.context, meta, testimonials.render)
     .get('/faq', faq.context, meta, faq.render)
-    .get('/programs/services', PDAndSupport.context, meta, PDAndSupport.render);
+    .get('/program/services', PDAndSupport.context, meta, PDAndSupport.render);
 
 // .get('/programs/accelerated-programs', accelerated.context, meta, accelerated.render)
 // .get('/about', about.context, meta, about.render)
