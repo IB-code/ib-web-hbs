87fec432875c86ea4d235b7a48f7eda2f960cb33
joshua-hurn <joshua.hurn@hotmail.com>
2021-11-08 08:41:13 -0600

    staging to show Amy

--- server/routes/alumni.ts
+++ server/routes/alumni.ts
@@ -18,7 +18,42 @@ export function context(
                 ],
             },
         },
-        main: {},
+        main: {
+            newsletters: [
+                {
+                    dateReleased: "October 2021",
+                    link: "https://app.donorview.com/Communication/View?prm=jtu-ucf-wYu5bemONAtGZtW22OEn8BkRWDMDVmrr-pLSHGP2dUrRoe-okiHszLimK8NTOjcnYDO-_SbcnHPXvbIvdFXShKTFYcQOrTvf3yFCMMqR9ExXA3sFPThbNU5Mn7lypsOp4FCH0u1XV0fUwlf46JPs1eRBaqNYHwC33ddiaxppbIe_hi86t0NXwF6bkv6dH1FFZN2Ot7IAVUuIFp0nNhbVGMCP0znji0oaRSk1"
+                },
+                {
+                    dateReleased: "September 2021",
+                    link: "https://app.donorview.com/Communication/View?prm=jtu-ucf-wYu5bemONAtGZtW22OEn8BkRWDMDVmrr-pLSHGP2dUrRoe-okiHszLimK8NTOjcnYDO-_SbcnHPXvbIvdFXShKTFYcQOrTvf3yGwAGLRf0m1wPqycKDemjSZ28IeA_gbpXJu_fc4EEkDDlouPwamcUeGC43mAbqfP9Q-P9GjWIrV94-psZjz3CMk3tYY5ibemRBy3VBrhhbOjezVPTY2-4VEkrrSX8PQOgk1"
+                },
+                {
+                    dateReleased: "August 2021",
+                    link: "https://app.donorview.com/Communication/View?prm=jtu-ucf-wYu5bemONAtGZtW22OEn8BkRWDMDVmrr-pLSHGP2dUrRoe-okiHszLimK8NTOjcnYDO-_SbcnHPXvbIvdFXShKTFYcQOrTvf3yHvbiwE_VDVVKE79qCNW-W507VeaA0lRo0gD5Wky7bwSxdLlKJvGxhAlGFqhUnRC9NBzrsrpUca4rZORX0b_FZfSUalrhF39zINMjwitgRAcjxbkqHOcsi4nc60BrT2-141"
+                },
+                {
+                    dateReleased: "July 2021",
+                    link: "https://app.donorview.com/Communication/View?prm=jtu-ucf-wYu5bemONAtGZtW22OEn8BkRWDMDVmrr-pLSHGP2dUrRoe-okiHszLimK8NTOjcnYDO-_SbcnHPXvbIvdFXShKTFYcQOrTvf3yE3J4jPzGrni8P1kZ4-d3yFa6IT6n3zEbmJQNBALEX2ZkwolYANcoQo8lN5yG4oTkKNW7_NIla3r9wzvA8ppkgeb1ZXGsOo1wksAVheKfgNGz_9dcJwxxXpAF1sdNoCvCs1"
+                },
+                {
+                    dateReleased: "June 2021",
+                    link: "https://app.donorview.com/Communication/View?prm=jtu-ucf-wYu5bemONAtGZtW22OEn8BkRWDMDVmrr-pLSHGP2dUrRoe-okiHszLimK8NTOjcnYDO-_SbcnHPXvbIvdFXShKTFYcQOrTvf3yEuU1HDaDLc7t8jl7P4KhTIXw39cRrf0AhXxWkhvSU7iNN1xWpYTlXTGQPikA8vVG6wV_u9QPodgqGcmCfwrhyI6A7w9-qeZQlmMzIARwiT9t_H8LVo0Kbqe31XrEnjUjM1"
+                },
+                {
+                    dateReleased: "May 2021",
+                    link: "https://app.donorview.com/Communication/View?prm=jtu-ucf-wYu5bemONAtGZtW22OEn8BkRWDMDVmrr-pLSHGP2dUrRoe-okiHszLimK8NTOjcnYDO-_SbcnHPXvbIvdFXShKTFYcQOrTvf3yGlQ-lZRvTThGCoHMUOzNJUDQvCCVjxvJxkDJVcvnu7HvL_MlkZWtfy5ci3znbBkzF8DF3v78npJRVniVsJ2Aej7OBnDvbIL-K3E35sNQWpayxzvgiOlqAkFiffq3sCXnw1"
+                },
+                {
+                    dateReleased: "April 2021",
+                    link: "https://app.donorview.com/Communication/View?prm=jtu-ucf-wYu5bemONAtGZtW22OEn8BkRWDMDVmrr-pLSHGP2dUrRoe-okiHszLimK8NTOjcnYDO-_SbcnHPXvbIvdFXShKTFYcQOrTvf3yH1mbyzyX2yu17OmOcGxUO9wt4d7qIibZOvjsu3nbsinvp4xPZFd2pz9-Bwd4b47KccD8I1cq727keifpoGqBNiFIGUNhIwCUwBMB1S321aRf9oPqfBmnrrbe_rX1iqH4I1"
+                },
+                {
+                    dateReleased: "March 2021",
+                    link: "https://app.donorview.com/Communication/View?prm=jtu-ucf-wYu5bemONAtGZtW22OEn8BkRWDMDVmrr-pLSHGP2dUrRoe-okiHszLimK8NTOjcnYDO-_SbcnHPXvbIvdFXShKTFYcQOrTvf3yFu4DArJFzGEr-al3gAZGbYEL2GVKm2TwQjlV2jmT0HhYZPGAZRoOOMNWGXf5eV9m_AbiBdPC6PXGZaebcfW9O_aVrXoeS0O8jTTo4vbhrJx0y9PTGH4AJzmfk4Xq3sfV01"
+                },
+            ]
+        },
     };
 
     next();
@@ -29,7 +64,7 @@ export function render(
     res: express.Response,
     next: express.NextFunction,
 ) {
-    res.render('alumni', req.context, (err, html) => {
+    res.render("alumni", req.context, (err, html) => {
         parse(req, req.url, html).then(
             (html) => {
                 res.send(html);
---
+++ ts/alumni.ts
@@ -0,0 +1,38 @@
+
+
+
+const setIframeContainer = (link: string) => {
+    const iframeContainer = <HTMLIFrameElement>document.querySelector('#iframe-container'),
+        newsletterHelper = <HTMLDivElement>document.querySelector('#newsletter-helper');
+
+    newsletterHelper.style.display = 'none';
+
+    const iframe = document.createElement('iframe');
+    iframe.setAttribute('src', link);
+    iframeContainer.innerHTML = '';
+    iframeContainer.appendChild(iframe);
+
+    console.log(this);
+};
+
+// const renderMobileNewsletterOptions = () => {
+//     const mobileNewsletterOptions = document.querySelector('#mobile-newsletter-options');
+
+//     if (screen.availWidth <= 576) {
+//         document.querySelector('#desktop-newsletter-options').remove();
+//         mobileNewsletterOptions.innerHTML = `
+//             <div class="dropdown">
+//                 <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+//                     Dropdown button
+//                 </button>
+//                 <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+//                     <a class="dropdown-item" href="#">Action</a>
+//                     <a class="dropdown-item" href="#">Another action</a>
+//                     <a class="dropdown-item" href="#">Something else here</a>
+//                 </div>
+//             </div>
+//         `;
+//     }
+// }
+
+// renderMobileNewsletterOptions();
