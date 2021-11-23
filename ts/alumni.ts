bfb0d37680ab74613434852d749034eec30071e6
InnovateBham <admin@InnovateBhams-MacBook-Pro-5.local>
2021-11-23 11:43:40 -0600

    fixed social icons

--- ts/alumni.ts
+++ ts/alumni.ts
@@ -1,6 +1,3 @@
-
-
-
 const setIframeContainer = (link: string) => {
     const iframeContainer = <HTMLIFrameElement>document.querySelector('#iframe-container'),
         newsletterHelper = <HTMLDivElement>document.querySelector('#newsletter-helper');
@@ -11,28 +8,26 @@ const setIframeContainer = (link: string) => {
     iframe.setAttribute('src', link);
     iframeContainer.innerHTML = '';
     iframeContainer.appendChild(iframe);
-
-    console.log(this);
 };
 
-// const renderMobileNewsletterOptions = () => {
-//     const mobileNewsletterOptions = document.querySelector('#mobile-newsletter-options');
+const renderMobileNewsletterOptions = () => {
+    const mobileNewsletterOptions = document.querySelector('#mobile-newsletter-options');
 
-//     if (screen.availWidth <= 576) {
-//         document.querySelector('#desktop-newsletter-options').remove();
-//         mobileNewsletterOptions.innerHTML = `
-//             <div class="dropdown">
-//                 <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-//                     Dropdown button
-//                 </button>
-//                 <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-//                     <a class="dropdown-item" href="#">Action</a>
-//                     <a class="dropdown-item" href="#">Another action</a>
-//                     <a class="dropdown-item" href="#">Something else here</a>
-//                 </div>
-//             </div>
-//         `;
-//     }
-// }
+    if (screen.availWidth <= 576) {
+        document.querySelector('#desktop-newsletter-options').remove();
+        mobileNewsletterOptions.innerHTML = `
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle btn-sm" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown button
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </div>
+        `;
+    }
+}
 
-// renderMobileNewsletterOptions();
+renderMobileNewsletterOptions();
