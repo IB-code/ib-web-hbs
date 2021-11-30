7271b6149f9498cf23948146c3159d725d29a9ef
joshua-hurn <joshua.hurn@hotmail.com>
2021-11-30 15:58:25 -0600

    fixed newsletter on alumni page

--- ts/alumni.ts
+++ ts/alumni.ts
@@ -10,24 +10,31 @@ const setIframeContainer = (link: string) => {
     iframeContainer.appendChild(iframe);
 };
 
-const renderMobileNewsletterOptions = () => {
+const renderMobileNewsletter = () => {
     const mobileNewsletterOptions = document.querySelector('#mobile-newsletter-options');
+    const desktopNewsletterOptions = document.querySelector('#desktop-newsletter-options');
 
     if (screen.availWidth <= 576) {
-        document.querySelector('#desktop-newsletter-options').remove();
-        mobileNewsletterOptions.innerHTML = `
-            <div class="dropdown">
-                <button class="btn btn-secondary dropdown-toggle btn-sm" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown button
-                </button>
-                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-            </div>
-        `;
+        // remove desktop newsletter options
+        desktopNewsletterOptions.remove();
+
+        // add mobile newsletter options
+        // mobileNewsletterOptions.innerHTML = `
+        //     <div class="dropdown">
+        //         <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        //             Dropdown button
+        //         </button>
+        //         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        //             ${
+
+        //             }
+        //         </div>
+        //     </div>
+        // `;
+    } else {
+        // remove mobile newsletter options
+        mobileNewsletterOptions.remove();
     }
 }
 
-renderMobileNewsletterOptions();
+renderMobileNewsletter();
