6cb5b3038651a24a88d2a16c0f8ed380eb198d3b
joshua-hurn <joshua.hurn@hotmail.com>
2022-04-29 11:29:41 -0500

    removed empty files in typescript compiled OLD files breaking minify process in build/minify

--- build/minify.js
+++ build/minify.js
@@ -26,6 +26,7 @@ utils
                 );
             })
             .then((output) => {
+                console.log(file, output)
                 return new Promise((resolve, reject) => {
                     fs.writeFile(file, output, (err) => {
                         if (!!err) {
